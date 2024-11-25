b2c64a0b8b9d6ed4842403946fbd904c15909ac8
mmmrrrmasha <maryusha2702@gmail.com>
2024-11-25 21:05:53 +0300

    Add files via upload

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -4,11 +4,15 @@ int main()
 {
 int k, i = 1, sum_k = 0, number;
 cout << " Enter the amount of integers to sum = "; cin >> k;
+if (k == 0)
+cout << " The total sum is 0";
+else {
 do {
 cout << "Enter integer nr. " << i << ": ";
 cin >> number;
 sum_k += number; i++;
 } while (i <= k);
 cout << " The total sum of " << k << " integers is: " << sum_k;
+}
 return 0;
 }
